1f533b6475fc96b5210ae1e3f8c4adf68ee7cc67
hadasat <atiyah@post.bgu.ac.il>
2017-04-27 20:38:44 +0300

    sss

--- src/Numeric.java
+++ src/Numeric.java
@@ -1,6 +1,19 @@
 
 public class Numeric implements Element{
 
+	private int number;
+
+	public void Numeric(int x){
+		number = x;
+	}
+
+	public int getNumber(){
+		return number;
+	}
+
+	public void setNumber(int x){
+		number = x;
+	}
 	public Numeric transformAdd(Numeric n){
 		return null;
 	}
